cde90dda84323866841c23d0adf485ee5ad30785
AndresMisaelbc <Andresmisaelbc@gmail.com>
2021-05-20 20:24:27 -0500

    Contacto mensajeria

--- INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Informacion.java
+++ INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Informacion.java
@@ -2,7 +2,9 @@ package com.example.insulita_pantallas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Informacion extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class Informacion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
     }
+public void SiguienteAtencion (View view)
+{
+    Intent siguienteatencionobj = new Intent(this,Mensajeria.class );
+    startActivity(siguienteatencionobj);
+}
+
+
 }
---
+++ INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Mensajeria.java
@@ -0,0 +1,47 @@
+package com.example.insulita_pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Mensajeria extends AppCompatActivity
+{
+    EditText etMsj, etCel;
+    Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensajeria);
+
+        etMsj = findViewById(R.id.editTextTextPersonName);
+        etCel = findViewById(R.id.editTextTextPersonName2);
+        btnEnviar = findViewById(R.id.button);
+
+        if (ActivityCompat.checkSelfPermission(Mensajeria.this, Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(Mensajeria.this, new String[]{Manifest.permission.SEND_SMS}, 1);
+        }
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SmsManager smsManager =  SmsManager.getDefault();
+                smsManager.sendTextMessage(etCel.getText().toString(), null, etMsj.getText().toString(), null, null);
+                Toast.makeText(Mensajeria.this, "MSJ ENVIADO", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
+
