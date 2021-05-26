de8a2d02d68c49f490b42c156a9dac25c3aedba4
AndresMisaelbc <Andresmisaelbc@gmail.com>
2021-05-26 14:09:00 -0500

    firebase sensor

--- INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/MainActivity.java
+++ INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/MainActivity.java
@@ -1,5 +1,12 @@
 package com.example.insulita_pantallas;
 
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,11 +15,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -22,17 +31,54 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
     //private Button fab;
     //private String url;
+    private TextView mtempview;
+    private TextView mhumview;
+    private TextView mdistview;
+    private DatabaseReference mDatabase;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        mtempview = (TextView) findViewById(R.id.tempview);
+        mhumview = (TextView) findViewById(R.id.humview);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("sensor-py").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String temp = snapshot.child("Temperatura").getValue().toString();
+                    String hum = snapshot.child("Humedad").getValue().toString();
+
+                    mtempview.setText("La temperatura del cuarto es de: " + temp + " C");
+                    mhumview.setText("La humedad del cuarto es de: " + hum);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 //
 //        fab = findViewById(R.id.fab);
 //        url="https://disqus.com/home/";
@@ -90,4 +136,7 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
 }
