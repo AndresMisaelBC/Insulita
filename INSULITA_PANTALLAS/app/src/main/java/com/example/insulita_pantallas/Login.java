d214b2d9dd546aec35489d8896b7b04c85ec5681
AndresMisaelbc <Andresmisaelbc@gmail.com>
2021-05-26 18:14:14 -0500

    Login y Registro

--- INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Login.java
+++ INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Login.java
@@ -1,14 +1,29 @@
 package com.example.insulita_pantallas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity
 {
+
+    private EditText edtEmail;
+    private EditText edtPassword;
+
+    private FirebaseAuth mAuth;
+
     Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,7 +31,44 @@ public class Login extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
+
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       // updateUI(currentUser);
+    }
+
+    public void IniciarSesion(View v)
+    {
+        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent siguienteRaMain = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(siguienteRaMain);
+                           // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                           // updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
     public void SiguienteLogin(View view)
     {
         Intent siguiente = new Intent(this,MainActivity.class);
@@ -29,5 +81,14 @@ public class Login extends AppCompatActivity
         startActivity(siguienteR);
 
     }
+
+    //public void IrIniciar(View view)
+    //{
+    //Intent i = new Intent(this, MainActivity.class);
+    //startActivity(i);
+
+    //}
+
+
 
 }
--- INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Registro.java
+++ INSULITA_PANTALLAS/app/src/main/java/com/example/insulita_pantallas/Registro.java
@@ -1,22 +1,74 @@
 package com.example.insulita_pantallas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registro extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText edt_email;
+    private EditText edt_password;
+    private EditText edt_confirm_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edt_email = findViewById(R.id.edt_email);
+        edt_password = findViewById(R.id.edt_password);
+        edt_confirm_password = findViewById(R.id.edt_confirm_password);
+
     }
-    public void Registro_Main(View view)
-    {
-        Intent siguienteRaMain = new Intent(this,MainActivity.class);
-        startActivity(siguienteRaMain);
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+
+    public void RegistrarUsuario(View v) {
+
+        if (edt_password.getText().toString().equals(edt_confirm_password.getText().toString())) {
+            mAuth.createUserWithEmailAndPassword(edt_email.getText().toString(), edt_password.getText().toString())
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+
+                            Toast.makeText(getApplicationContext(), "usuario creado", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent siguienteRaMain = new Intent(getApplicationContext(), Login.class);
+                            startActivity(siguienteRaMain);
+
+                        } else {
+
+
+                            Toast.makeText(getApplicationContext(), "Fallo la autentificacion.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
+
+
